439ce323f6960fd49a6f2824083545da0e60785b
campagnamatias <campagnamatias@gmail.com>
2022-09-06 13:31:51 -0300

    agrego perspectiva a otros elementos

--- animation.js
+++ animation.js
@@ -1,9 +1,13 @@
-const wrapper = document.querySelector('.container');
+const container = document.querySelector('.container');
 const ticket = document.querySelector('.ticket');
+const titulo = document.querySelector('.vite-data h2');
+const userBox = document.querySelector('.user-box');
 
-wrapper.addEventListener('mousemove', event =>{
+container.addEventListener('mousemove', event =>{
     let xAxis = (event.pageX - window.innerWidth / 2) / 50;
     let yAxis = (window.innerHeight / 2 - event.pageY) / 50;
 
     ticket.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    titulo.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg) perspective(500px) translateZ(50px)`;
+    userBox.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg) perspective(500px) translateZ(50px)`;
 })
